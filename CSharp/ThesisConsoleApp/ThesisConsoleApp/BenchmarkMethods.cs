2655a8fe9cdacb3c59ca55eb8b2d2bce942813bf
Türhan Yıldırım <yildirimturhan@gmail.com>
2022-04-24 01:38:46 +0300

    Update.

---
+++ CSharp/ThesisConsoleApp/ThesisConsoleApp/BenchmarkMethods.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics;
+
+namespace ThesisConsoleApp
+{
+    internal class BenchmarkMethods
+    {
+        private string SampleFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Files//Sample.txt");
+
+        public void ReadFileSyncFlowBenchmark()
+        {
+            Console.WriteLine(String.Empty);
+            Console.WriteLine("File Read Sync Flow Started");
+            var times = new List<long>();
+            var fileReadSyncFlow = new Stopwatch();
+            for (int i = 1; i <= 10; i++)
+            {
+                fileReadSyncFlow.Reset();
+                fileReadSyncFlow.Start();
+                for (int t = 0; t < 1000; t++)
+                {
+                    ReadFile();
+                }
+                fileReadSyncFlow.Stop();
+                var elapsedTime = fileReadSyncFlow.ElapsedMilliseconds;
+                times.Add(elapsedTime);
+                Console.WriteLine($"File Read Sync Flow Complete Miliseconds({i}): {elapsedTime}");                
+            }
+            Console.WriteLine($"File Read Sync Flow Completed > Average: {Queryable.Average(times.AsQueryable())}");
+            Console.WriteLine(String.Empty);
+            Console.WriteLine(String.Empty);
+        }
+
+        public void CalculateSyncFlowBenchmark()
+        {
+            Console.WriteLine(String.Empty);
+            Console.WriteLine("Calculate Sync Flow Started");
+            var times = new List<long>();
+            var fileReadSyncFlow = new Stopwatch();
+            for (int i = 1; i <= 10; i++)
+            {
+                fileReadSyncFlow.Reset();
+                fileReadSyncFlow.Start();
+                for (int t = 0; t < 1000; t++)
+                {
+                    ReadFile();
+                }
+                fileReadSyncFlow.Stop();
+                var elapsedTime = fileReadSyncFlow.ElapsedMilliseconds;
+                times.Add(elapsedTime);
+                Console.WriteLine($"Calculate Sync Flow Complete Miliseconds({i}): {elapsedTime}");
+            }
+            Console.WriteLine($"Calculate Sync Flow Completed > Average: {Queryable.Average(times.AsQueryable())}");
+            Console.WriteLine(String.Empty);
+            Console.WriteLine(String.Empty);
+        }
+
+        public void ReadFileAsyncFlowBenchmark()
+        {
+            Console.WriteLine(String.Empty);
+            Console.WriteLine("File Read Async Flow Started");            
+            
+            var times = new List<long>();
+            var fileReadSyncFlow = new Stopwatch();
+            for (int i = 1; i <= 10; i++)
+            {
+                var tasks = new List<Task>();
+                fileReadSyncFlow.Reset();
+                fileReadSyncFlow.Start();
+                for (int t = 0; t < 1000; t++)
+                {
+                    tasks.Add(Task.Run(ReadFile));
+                }
+                Task.WaitAll(tasks.ToArray());
+                fileReadSyncFlow.Stop();
+                var elapsedTime = fileReadSyncFlow.ElapsedMilliseconds;
+                times.Add(elapsedTime);
+                Console.WriteLine($"File Read Async Flow Complete Miliseconds({i}): {elapsedTime}");
+            }
+            Console.WriteLine($"File Read Async Flow Completed > Average: {Queryable.Average(times.AsQueryable())}");
+            Console.WriteLine(String.Empty);
+            Console.WriteLine(String.Empty);
+        }
+
+        public void CalculateAsyncFlowBenchmark()
+        {
+            Console.WriteLine(String.Empty);
+            Console.WriteLine("Calculate Async Flow Started");
+
+            var times = new List<long>();
+            var fileReadSyncFlow = new Stopwatch();
+            for (int i = 1; i <= 10; i++)
+            {
+                var tasks = new List<Task>();
+                fileReadSyncFlow.Reset();
+                fileReadSyncFlow.Start();
+                for (int t = 0; t < 1000; t++)
+                {
+                    tasks.Add(Task.Run(Calculate));
+                }
+                Task.WaitAll(tasks.ToArray());
+                fileReadSyncFlow.Stop();
+                var elapsedTime = fileReadSyncFlow.ElapsedMilliseconds;
+                times.Add(elapsedTime);
+                Console.WriteLine($"Calculate Async Flow Complete Miliseconds({i}): {elapsedTime}");
+            }
+            Console.WriteLine($"Calculate Async Flow Completed > Average: {Queryable.Average(times.AsQueryable())}");
+            Console.WriteLine(String.Empty);
+            Console.WriteLine(String.Empty);
+        }
+
+
+        private void ReadFile()
+        {
+            var content = File.ReadAllText(SampleFilePath);
+        }
+
+        private void Calculate()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var result = Math.Sqrt(i);
+            }
+        }
+    }
+}
---
+++ CSharp/ThesisConsoleApp/ThesisConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ThesisConsoleApp;
+
+Console.WriteLine("Sync Code Execution (FileRead and Calculate will run 100 times)");
+Console.WriteLine("---------------------------------------------------------------");
+Console.WriteLine(string.Empty);
+
+var methods = new BenchmarkMethods();
+
+methods.ReadFileSyncFlowBenchmark();
+methods.CalculateSyncFlowBenchmark();
+
+methods.CalculateAsyncFlowBenchmark();
+methods.ReadFileAsyncFlowBenchmark();
